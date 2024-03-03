19838763b84fba70b5e0200a2be64637faf95ca9
zeid <zeidhassan4@gmail.com>
2024-03-03 16:34:04 +0300

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def  __init__(self, name, breed = "Mutt"):    #The breed will  be set to Mutt if not provided.
+        self.name = name
+        self.breed = breed
+fido = Dog("Fido", "Dalmatian")  #The breed will be set  to Dalmatian(the argument passed) for Fido.
+
+print(f"{fido.name} is a {type(fido).__name__} and the breed is of a  {fido.breed}.")
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self,  name):
+        self.name = name
+guido = Person("Guido")
+jython = Person("Jython")
+
+print(f"{guido.name} and {jython.name} are friends.")
+
